1edab6e43ce585009ef2765c3e1f0b9ee14766c0
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-25 16:35:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if position_taken(board,index)
+  if (position_taken(board,index) || index.between?(0,8))
     #position taken is true
     return false
 
