85ca6cee86e83d9161e260aecd5f3b25632ab811
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-25 16:31:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,11 +2,6 @@
 def valid_move?(board,index)
   if(index.between(0,8))
 
-    if ((board[index] == " ") || (board[index] == "") || (board[index] == nil))
-      false
-    elsif ((board[index] == "X") || (board[index] == "O"))
-      true
-    end
 
   else
 
